58456ca5e7ed6444b385d1184250a65fa388b451
grey <youth6966@gmail.com>
2024-04-02 10:27:09 +0800

    publicPath: '/test20240328/'

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="This is a test project again 123"/>
+    <HelloWorld msg="Some Ting to change"/>
   </div>
 </template>
 
